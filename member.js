013adc779125ebf5fc79a387dd283c6ea2b82ccc
Riya Zulphiker Ali <70123334+riyazulphikeraliev@users.noreply.github.com>
2024-05-09 13:23:34 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember(){
+    // Get the member
+    var member = getMember();
+    // Get the skills of the member
+    var skills = member.skills;
+    // Get the skills of the member
+    return skills;
+}
